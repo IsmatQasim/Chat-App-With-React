98a9f35ae3cb4ce760623ba8d131bc7f76cbf1b5
IsmatQasim <ismat6812@gmail.com>
2024-02-29 14:41:09 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { ChatEngine } from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed';
+import './App.css';
 const App = () => {
   return (
-    <div>App</div>
+    <ChatEngine 
+        height="100vh"
+        projectID=" 2db135e3-a42f-4341-9adf-d6b32c82291a"
+        userName="IsmatScripts__"
+        userSecret="1234"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+
+    />
   )
 }
 
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import MessageForm from './MessageForm'
+import MyMessage from './MyMessage'
+import TheirMessage
+ from './TheirMessage'
+const ChatFeed = (props) => {
+    const{ chats, activeChat , userName , messages} = props;
+
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () => {
+        const keys= Object.keys(messages);
+
+        return keys.map((key, index) =>{
+            const message = messages[key]
+            const lastMessagekey = index == 0 ? null : keys[index -1]
+            const isMyMessage = userName == message.sender.username;
+        })
+    }
+    renderMessages()
+  return (
+    <div>ChatFeed</div>
+  )
+}
+
+export default ChatFeed
