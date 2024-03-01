aa3c77ad79b4bb09f7214ca350a8a7a6ef7ba199
IsmatQasim <ismat6812@gmail.com>
2024-03-01 14:34:52 +0500

    commit

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -2,18 +2,30 @@ import React from 'react'
 import MessageForm from './MessageForm'
 import MyMessage from './MyMessage'
 import TheirMessage from './TheirMessage'
+
 const ChatFeed = (props) => {
     const{ chats, activeChat , userName , messages} = props;
 
     const chat = chats && chats[activeChat];
+
+   const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
+    <div
+      key={`read_${index}`}
+      className="read-receipt"
+      style={{
+        float: isMyMessage ? 'right' : 'left',
+        backgroundImage: person.person.avatar && `url(${person.person.avatar})`
+      }}
+    />
+  ));
 
     const renderMessages = () => {
         const keys= Object.keys(messages);
 
         return keys.map((key, index) =>{
             const message = messages[key]
-            const lastMessagekey = index == 0 ? null : keys[index -1]
-            const isMyMessage = userName == message.sender.username;
+            const lastMessagekey = index === 0 ? null : keys[index -1]
+            const isMyMessage = userName === message.sender.username;
 
             return(
                 <div key={`msg_${index}`} style={{width: '100%'}}>
@@ -24,13 +36,12 @@ const ChatFeed = (props) => {
                             : <TheirMessage message={message}  lastMessage= {messages[lastMessagekey]}/>
                         }
                     </div>
-                    <div className='read-receipts' style={{ marginRight: isMyMessage ? '18px': '0px', marginLeft: isMyMessage? '0px' : '68px'}}></div>
-                    read-receipts
+                    <div className='read-receipts' style={{ marginRight: isMyMessage ? '18px': '0px', marginLeft: isMyMessage? '0px' : '68px'}}>
+                    {renderReadReceipts(message, isMyMessage)}
+                    </div>
                 </div>
-
-
-            )
-        })
+                      )
+    })
     }
 
 
@@ -39,7 +50,7 @@ const ChatFeed = (props) => {
     <div className='chat-feed'>
         <div className='chat-title-container'>
             <div className='chat-title'> 
-            {chat.title}
+            {chat?.title}
             </div>
             <div className= "chat-subtitle">
                 {chat.people.map((person) => `${person.person.username}`)}
