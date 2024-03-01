bd1235a1276c4bbe2341b34c5a54d5529e88e60a
IsmatQasim <ismat6812@gmail.com>
2024-03-01 12:32:05 +0500

    changes

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
-const TheirMessage = () => {
+const TheirMessage = ({ lastmessage, message}) => {
+    const isFirstMessageByUser = !lastmessage || lastmessage.sender.username !== message.sender.username
   return (
-    <div>TheirMessage</div>
+    <div className='message-row'>
+        {isFirstMessageByUser &&(
+            <div 
+            className='message-avatar'
+            style={{backgroundImage: `url(${message?.sender?.avatar})`}}
+            />
+        ) }
+       {message?.attachments?.length > 0 
+        ? (
+            <img 
+            src={message.attachments[0].file}
+            alt='message-attachment'
+            className='message-image'
+            style={{marginLeft: isFirstMessageByUser ? '4px' : '48px'}}
+            />
+            ) : (
+                <div className='message' style={{float: 'left', backgroundColor:'#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px'}}>
+                    {message.text}
+                </div>
+            )
+    }
+   </div>
   )
 }
 
